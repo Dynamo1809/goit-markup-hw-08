557d3856663fddbaa9e493a877cf20d48333f5df
Dmytro Maksiuta <d.maksiuta18@gmail.com>
2021-12-27 13:58:00 +0200

    add overflow hidden to body class

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,13 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
   const navContactsLinkRef = document.querySelector('.nav-contacts-link'); 
   navContactsLinkRef.addEventListener("click", () => {
-    document.body.classList.toggle("fixed");
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
+    if (menuBtnRef.getAttribute("aria-expanded") === "true") {
+      document.body.classList.toggle("fixed");
+      const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+      mobileMenuRef.classList.toggle("is-open");
+    } 
   })
   menuBtnRef.addEventListener("click", () => {
     document.body.classList.toggle("fixed");
